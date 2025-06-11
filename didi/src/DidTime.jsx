7586d328a5667c16a5727603458671189d301376
studentKim2000 <166790301+studentKim2000@users.noreply.github.com>
2025-06-11 23:07:19 +0900

    2개의 시간 보여주기

--- didi/src/DidTime.jsx
+++ didi/src/DidTime.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 
 function DidTime( {title }) {
-  const [did_time, set_did_time] = useState("");
+  const [did_times, set_did_times] = useState([]);
 
   function getdate()
   {
@@ -11,8 +11,20 @@ function DidTime( {title }) {
 
   function checkTime()
   {
-    let current_time = getdate()
-    set_did_time(current_time)
+    const current_time = getdate();
+    // 전체 개수가 1개 이하면 배열에 현재 시간을 추가한다.
+    if (did_times.length <= 1)
+    {
+      const new_did_times = [...did_times, current_time];
+      console.log("did_times :", new_did_times);
+      set_did_times(new_did_times.sort().reverse());
+    }
+    else // 전체 개수가 2개면 2번째 배열의 값은 지금 시간으로 변경한다.
+    {
+      const new_did_times = [...did_times];
+      new_did_times[1] = current_time;      
+      set_did_times(new_did_times.sort().reverse());
+    }
   }
 
   return (
@@ -22,7 +34,12 @@ function DidTime( {title }) {
           <button onClick={checkTime}>{title}</button>
         </div>
         <div>
-          {did_time}
+          {
+            did_times.map( (item) => (
+                <div>{item}</div>
+              )
+            )
+          }
         </div>
       </div>
     </>
