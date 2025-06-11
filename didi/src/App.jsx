0979e52a41341ae38c54804dd6368122646c23cb
studentKim2000 <166790301+studentKim2000@users.noreply.github.com>
2025-06-11 17:13:58 +0900

    날짜 시각 보여주기

--- didi/src/App.jsx
+++ didi/src/App.jsx
@@ -1,8 +1,13 @@
-import './App.css'
-
 function App() {
+  function getdate()
+  {
+    const date = new Date();    
+    return date.toLocaleDateString('ko-KR') + ' ' +  date.toLocaleTimeString('ko-KR');
+  }
+
   return (
-    <>
+    <>      
+      {getdate()}
     </>
   )
 }
