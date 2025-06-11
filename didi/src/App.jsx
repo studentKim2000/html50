fccda5e382394376a79d093742eb1acc3e733d1d
studentKim2000 <166790301+studentKim2000@users.noreply.github.com>
2025-06-11 21:49:28 +0900

    버튼 누르면 시간 표시

--- didi/src/App.jsx
+++ didi/src/App.jsx
@@ -1,14 +1,30 @@
+import {useState} from 'react';
+
 function App() {
+  const [did_time, set_did_time] = useState("");
+
   function getdate()
   {
     const date = new Date();    
     return date.toLocaleDateString('ko-KR') + ' ' +  date.toLocaleTimeString('ko-KR');
   }
 
+  function checkTime()
+  {
+    let current_time = getdate()
+    set_did_time(current_time)
+  }
+
   return (
     <>
-      <button>출/퇴근</button>
-      {getdate()}
+      <div className="center_div">
+        <div>
+          <button onClick={checkTime}>출/퇴근</button>
+        </div>
+        <div>
+          {did_time}
+        </div>
+      </div>
     </>
   )
 }
