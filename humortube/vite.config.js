c726de1bfb7a0dab013684a1a7c44c0acdddc329
studentKim2000 <166790301+studentKim2000@users.noreply.github.com>
2025-08-07 14:34:24 +0900

    50으로 맞춰서

--- humortube/vite.config.js
+++ humortube/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/html5/humortube/',
+  base: '/html50/humortube/',
   plugins: [react()],
 })
